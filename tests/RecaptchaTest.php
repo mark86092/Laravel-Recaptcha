48b631d28503b958618644a93549a06cf63b56d6
Yen-chi Chen <mark86092@gmail.com>
2016-04-27 08:21:47 +0800

    Use namespace, simplify test

--- tests/RecaptchaTest.php
+++ tests/RecaptchaTest.php
@@ -1,7 +1,11 @@
 <?php
 
+namespace Mark86092\Tests\Recaptcha;
+
 use Mockery as m;
 use Mark86092\Recaptcha\Recaptcha;
+use ReCaptcha\ReCaptcha as GoogleReCaptcha;
+use ReCaptcha\Response as GoogleReCaptchaResponse;
 
 class RecaptchaTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,9 +16,10 @@ class RecaptchaTest extends \PHPUnit_Framework_TestCase
 
     public function testRecaptchaCanCheck()
     {
-        $provider = m::mock(\ReCaptcha\ReCaptcha::class);
-        $response = new \ReCaptcha\Response(true, []);
-        $provider->shouldReceive('verify')->once()->andReturn($response);
+        $provider = m::mock(GoogleReCaptcha::class);
+        $provider->shouldReceive('verify')->once()->andReturn(
+            new GoogleReCaptchaResponse(true, [])
+        );
 
         $captcha = new Recaptcha($provider);
         $this->assertTrue($captcha->verify('secret')->check());
